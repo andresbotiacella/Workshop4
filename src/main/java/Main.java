1351ea6f3c84ac1ce92e4630896e32163c2f0b00
Rafael <ra.botia@uniandes.edu.co>
2016-03-17 22:00:08 -0500

    ui fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,9 +33,9 @@ public class Main {
         SizeRange result1 = new SizeRange();
         data1 = controller.loadClassInfo("List1.txt");
         String htmlData = "Test Case # 1<br><br>"; //Title
-        htmlData += "<div>";
-        htmlData += "<table style=\"float: left; border-collapse: collapse; text-align: center\">"; //Open Table
-        htmlData += "<tr><th style=\"width: 150px;\">Class Name</th><th style=\"width: 60px;\">LOC</th><th style=\"width: 60px;\">Items</th></tr>"; //Header
+        htmlData += "<div style=\"display: inline-flex\">";
+        htmlData += "<table style=\"border: 1px solid; border-collapse: collapse; text-align: center\">"; //Open Table
+        htmlData += "<tr><th style=\"border: 1px solid; width: 150px;\">Class Name</th><th style=\"border: 1px solid; width: 60px;\">LOC</th><th style=\"border: 1px solid; width: 60px;\">Items</th></tr>"; //Header
         for(SizeInfo classData : data1) 
         {
             htmlData += String.format("<tr><td>%s</td><td>%s</td><td>%s</td></tr>", classData.getClassName(), classData.getLoc(), classData.getNumberOfMethods());
@@ -43,7 +43,7 @@ public class Main {
         htmlData += "</table><br>"; //Close Table
         result1 = controller.calculateSizeRange(data1);
         htmlData += String.format("<p style=\"float:left; margin-left: 20px\">Very Small = %.5g%n LOCs/Method<br>Small =  %.5g%n LOCs/Method<br>Medium = %.4g%n LOCs/Method<br>Large = %.4g%n LOCs/Method<br>Very Large = %.4g%n LOCs/Method<br></p>", result1.getVerySmall(), result1.getSmall(), result1.getMedium(), result1.getLarge(), result1.getVeryLarge());
-        htmlData += "</div><br><br>";
+        htmlData += "</div><br><br><br>";
         
         
         //Test Case 2
@@ -51,8 +51,8 @@ public class Main {
         SizeRange result2 = new SizeRange();
         data2 = controller.loadClassInfo("List2.txt");
         htmlData += "Test Case # 2<br><br>"; //Title
-        htmlData += "<div>";
-        htmlData += "<table style=\"border-collapse: collapse; text-align: center\">"; //Open Table
+        htmlData += "<div style=\"display: inline-flex\">";
+        htmlData += "<table style=\"borderborder-collapse: collapse; text-align: center\">"; //Open Table
         htmlData += "<tr><th style=\"border: 1px solid; width: 120px;\">Chapter</th><th style=\"border: 1px solid; width: 60px;\">Pages</th><th style=\"border: 1px solid; width: 60px;\">Items</th></tr>"; //Header
         for(SizeInfo chapterData : data2) 
         {
