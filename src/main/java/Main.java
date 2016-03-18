23d3d7470b44ea18af6af65153d6ea7015cad32d
Rafael <ra.botia@uniandes.edu.co>
2016-03-17 19:20:47 -0500

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,6 +20,14 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
     
+    get("/", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello World!");
+
+            return new ModelAndView(attributes, "index.ftl");
+        }, new FreeMarkerEngine());
+    
+    
     get("/calculateSize", (req, res) -> {
         
         String[] FILE_NAMES = {"List1.txt", "List2.txt"};
