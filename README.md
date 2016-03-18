dd9a16006468e2276884ba6c9a73d02b0e64449b
Rafael <ra.botia@uniandes.edu.co>
2016-03-17 19:08:33 -0500

    fixes

--- README.md
+++ README.md
@@ -1,28 +1,27 @@
-# Relative Size Calculation
+-- Relative Size Calculator
 
-Program to calculate relative size ranges for very small, small, medium, large, and very large ranges using standard deviation.
 
-## Running Locally
+1. Clone from GITHUB. 
 
-Make sure you have Java and Maven installed.  Also, install the [Heroku Toolbelt](https://toolbelt.heroku.com/).
+To clone execute the following commands:
+--
+$ git clone https://github.com/andresbotiacella/Workshop4
+$ cd Workshop4
+--
 
-```sh
-$ git clone https://github.com/sanarroya/ECOS-Relative-Size-Calculation.git
-$ cd Tarea4
+2. Compile and run tests with MAVEN.
+
+--
 $ mvn clean install
+--
+
+3. Access program in Heroku
+
+To access the application locally:
+--
 $ heroku local web
-```
+--
+Open browser on [localhost:5000](http://localhost:5000/).
 
-Your app should now be running on [localhost:5000](http://localhost:5000/).
-
-If you're going to use a database, ensure you have a local `.env` file that reads something like this:
-
-```
-DATABASE_URL=postgres://localhost:5432/java_database_name
-```
-
-## Heroku
-
-To access the application on heroku go to the following link:
-
-- [Relative size calculation](https://hidden-brook-36984.herokuapp.com/)
+To access the application on heroku:
+Open browser and go to: (https://pure-retreat-56211.herokuapp.com/calculateSize)
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,14 +19,10 @@ public class Main {
 
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
-
-    get("/hello", (req, res) -> "Hello World");
     
-    get("/calculateValues", (req, res) -> {
+    get("/calculateSize", (req, res) -> {
         
-        final String FILE_NAME_1 = "List1.txt";
-        final String FILE_NAME_2 = "List2.txt";
-        final String[] FILE_NAMES = {FILE_NAME_1, FILE_NAME_2};
+        String[] FILE_NAMES = {"List1.txt", "List2.txt"};
         List<SizeInfo> data;
         SizeController controller = new SizeController();
         SizeRange result = new SizeRange();
@@ -51,39 +47,6 @@ public class Main {
         }
         return dataString;
     });
-    
-    get("/", (request, response) -> {
-            Map<String, Object> attributes = new HashMap<>();
-            attributes.put("message", "Hello World!");
-
-            return new ModelAndView(attributes, "index.ftl");
-        }, new FreeMarkerEngine());
-
-    get("/db", (req, res) -> {
-      Connection connection = null;
-      Map<String, Object> attributes = new HashMap<>();
-      try {
-        connection = DatabaseUrl.extract().getConnection();
-
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-        ArrayList<String> output = new ArrayList<String>();
-        while (rs.next()) {
-          output.add( "Read from DB: " + rs.getTimestamp("tick"));
-        }
-
-        attributes.put("results", output);
-        return new ModelAndView(attributes, "db.ftl");
-      } catch (Exception e) {
-        attributes.put("message", "There was an error: " + e);
-        return new ModelAndView(attributes, "error.ftl");
-      } finally {
-        if (connection != null) try{connection.close();} catch(SQLException e){}
-      }
-    }, new FreeMarkerEngine());
 
   }
 
